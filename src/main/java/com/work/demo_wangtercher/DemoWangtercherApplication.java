79c3072d0613b0d55d86b748393a7b67f7b1e797
ovo <56571777+xuBlackDog@users.noreply.github.com>
2019-11-28 19:23:53 +0800

    Update DemoWangtercherApplication.java

--- src/main/java/com/work/demo_wangtercher/DemoWangtercherApplication.java
+++ src/main/java/com/work/demo_wangtercher/DemoWangtercherApplication.java
@@ -2,7 +2,8 @@ package com.work.demo_wangtercher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//这是扫描注解
+@MapperScanner
 @SpringBootApplication
 public class DemoWangtercherApplication {
 
