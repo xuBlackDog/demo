8316292921819633685563495f27cf1c3cbb26dd
xuBlackDog <1325914621@qq.com>
2019-11-28 18:23:35 +0800

    这是王老狗

---
+++ src/main/java/com/work/demo_wangtercher/DemoWangtercherApplication.java
@@ -0,0 +1,13 @@
+package com.work.demo_wangtercher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWangtercherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWangtercherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/work/demo_wangtercher/DemoWangtercherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.demo_wangtercher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWangtercherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
